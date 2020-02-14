6acd25278e186df91df568956dcb962fc088d163
Chris Blackburn <krizboy@vt.edu>
2020-02-14 14:08:19 -0500

    transfering lost commit to new repo

---
+++ external/bsd/ntp/dist/sntp/version.c
@@ -0,0 +1,7 @@
+/*	$NetBSD: version.c,v 1.1.1.9.4.1 2018/04/11 02:58:44 msaitoh Exp $	*/
+
+/*
+ * version file for sntp
+ */
+#include <config.h>
+const char * Version = "sntp 4.2.8p11@1.3728-o Tue Feb 27 22:59:12 UTC 2018 (50)";
---
+++ tools/gcc/gcc-version.mk
@@ -0,0 +1,5 @@
+#	$NetBSD: gcc-version.mk,v 1.7.4.2 2018/09/05 08:59:04 martin Exp $
+
+# common location for tools and native build
+
+NETBSD_GCC_VERSION=nb3 20180905
